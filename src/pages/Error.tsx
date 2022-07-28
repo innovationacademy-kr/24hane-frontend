41b764791b6d36053d4dd50dc82a00c41cf47ea4
6mn12j <dbwk0404@hanmail.net>
2022-07-28 20:57:22 +0900

    [#5] 에러페이지 추가

---
+++ src/pages/Error.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import classes from "styles/components/MainHeader.module.css";
+
+type ErrorProps = {
+  statusCode?: string;
+  message?: string;
+};
+
+const Error = ({ statusCode = "404", message = "notFound" }: ErrorProps) => {
+  return (
+    <>
+      <span className={classes.logo}>42 logo</span>
+      <div>
+        <h1 className={classes.title}>
+          {statusCode}
+          <br></br>
+          {message} ErrorPage
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default Error;
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -3,6 +3,7 @@ import Admin from "pages/Admin";
 import Main from "pages/Main";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "pages/Home";
+import Error from "pages/Error";
 
 const AppRouter = () => {
   return (
@@ -11,6 +12,7 @@ const AppRouter = () => {
         <Route path='/' element={<Home />} />
         <Route path='/main' element={<Main />} />
         <Route path='/admin' element={<Admin />} />
+        <Route path='*' element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
