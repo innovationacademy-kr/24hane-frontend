51fe9446702811b15570e4d2c26ef93352a353f0
6mn12j <dbwk0404@hanmail.net>
2022-08-11 23:14:41 +0900

    Feat: 센트리 dev 환경 테스트

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,10 @@ import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
+    {/* <Sentry.ErrorBoundary fallback={<p>에러가 발생 했습니다.</p>}> */}
     <Provider store={store}>
       <App />
     </Provider>
+    {/* </Sentry.ErrorBoundary> */}
   </React.StrictMode>,
 );
