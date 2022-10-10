a092841cd016394cc505814938d4ea0b2beee7bf
6mn12j <dbwk0404@hanmail.net>
2022-10-10 17:32:00 +0900

    Fix: 로그인 시 리다이렉트 고침 (#30)
    - 변경 전 기존의 주소창에 main을 추가하는 부분을 origin으로 변경하였습니다.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
   const handleLogin = () => {
     window.location.href = `${
       process.env.REACT_APP_API_URL
-    }/user/login/42?redirect=${encodeURIComponent(window.location.href)}main`;
+    }/user/login/42?redirect=${encodeURIComponent(window.location.origin)}/main`;
   };
 
   return (
