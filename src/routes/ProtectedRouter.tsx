c2e43637a400beb4db9ac3b020a1a0118e16b152
6mn12j <dbwk0404@hanmail.net>
2022-10-24 16:29:02 +0900

    Docs: ProtectedRoute jsDoc 추가(#13)

--- src/routes/ProtectedRouter.tsx
+++ src/routes/ProtectedRouter.tsx
@@ -13,13 +13,19 @@ const isLogin = (status: number = 401) => {
   return status === STATUS_204_NO_CONTENT;
 };
 
-export const ProtectedRoute = ({ redirectPath, authIsRequired, children }: ProtectedRouteProps) => {
+/**
+ * @param authIsRequired children컴포넌트가 auth가 필요한지 여부
+ * @param redirectPath auth 조건에 맞지 않을때 리다이렉트 될 경로
+ * @param children Route path에 따라 렌더링 될 컴포넌트
+ * @returns 라우팅 된 컴포넌트
+ */
+
+export const ProtectedRoute = ({ authIsRequired, redirectPath, children }: ProtectedRouteProps) => {
   const {
     data,
     queryInfo: { isSuccess },
   } = useIsLoginQuery();
 
-  console.log(data?.status);
   if (isSuccess && isLogin(data?.status) === authIsRequired) return children;
   else return <Navigate to={redirectPath} />;
 };
