24d43ef599e0dd52ed5d8f9311a984af6d6cd4b4
42inshin <inshin@student.42seoul.kr>
2024-01-08 13:15:23 +0900

    [FE] FIX: removeCookie 삭제 도메인 변경 #86

--- src/api/cookie/cookies.ts
+++ src/api/cookie/cookies.ts
@@ -1,5 +1,4 @@
 const tokenName = import.meta.env.VITE_TOKEN;
-const domain = new URL(window.location.origin).hostname;
 
 export const getCookie = () => {
   return document.cookie
@@ -11,5 +10,6 @@ export const getCookie = () => {
 };
 
 export const removeCookie = (): void => {
+  const domain = ".24hoursarenotenough.42seoul.kr";
   document.cookie = `${tokenName}=; path=/; domain=${domain}; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
 };
